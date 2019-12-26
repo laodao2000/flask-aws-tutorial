be5ab40bba8db5688734141bcab8a56220b9eca6
laodao2000 <39816138+laodao2000@users.noreply.github.com>
2019-12-26 13:27:02 -0500

    Updated RDS endpoint

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:testtest@my-handwriting-db.cyhghdpristx.us-east-2.rds.amazonaws.com:3306/user-handwriting'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +11,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = 'hVEhxQy8qGJTit6qATnaXaadqgNRsnglgs32Yayj'
